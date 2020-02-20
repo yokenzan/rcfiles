867ced8cd5ad74b77a9b7364ffbf42a0d535feae
yokenzan <31175068+yokenzan@users.noreply.github.com>
2020-02-20 21:48:36 +0900

    fix wrong path handling in setup.sh

--- setup.sh
+++ setup.sh
@@ -26,24 +26,28 @@ ln -sf $(pwd)/.bash_functions     $HOME
 ln -sf $(pwd)/.inputrc            $HOME
 ln -sf $(pwd)/.editrc             $HOME
 
-# Git
-
-mkdir -p ~/.config/git/completion
-cd ~/.config/git/completion
-
-GIT_VERSION=v$(git --version | sed -e ' s/.* //')
-
-curl -sS -O https://raw.githubusercontent.com/git/git/$GIT_VERSION/contrib/completion/git-completion.bash
-curl -sS -O https://raw.githubusercontent.com/git/git/$GIT_VERSION/contrib/completion/git-prompt.sh
-
-# Tig
-
-ln -sf $(pwd)/.tigrc              $HOME
-cp -f  $(pwd)/.gitconfig.example  $HOME/.gitconfig
-
 # Vim
 
 mkdir -p $HOME/.vim/{undo,backup,swap}
 
 ln -sf $(pwd)/.vimrc              $HOME
 ln -sf $(pwd)/.vimrc.colorscheme  $HOME
+
+# Git
+
+mkdir -p ~/.config/git/completion
+
+GIT_VERSION=v$(git --version | sed -e ' s/.* //')
+
+curl -sS \
+    -O $HOME/.config/git/completion/git-completion.bash \
+    https://raw.githubusercontent.com/git/git/$GIT_VERSION/contrib/completion/git-completion.bash
+curl -sS \
+    -O $HOME/.config/git/completion/git-prompt.sh \
+    https://raw.githubusercontent.com/git/git/$GIT_VERSION/contrib/completion/git-prompt.sh
+
+# Tig
+
+ln -sf $(pwd)/.tigrc              $HOME
+cp -f  $(pwd)/.gitconfig.example  $HOME/.gitconfig
+
